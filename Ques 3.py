85087b545bcfb686ae4394767b1777fa05acb49b
ayushaj-16 <56079780+ayushaj-16@users.noreply.github.com>
2019-10-02 19:45:05 +0530

    Delete Ques 3.py

--- Ques 3.py
+++
@@ -1,9 +0,0 @@
-print("Enter three numbers")
-x=int(input())
-y=int(input())
-z=int(input())
-A=max(x,y,z)
-B=min(x,y,z)
-print("The largest number amonngst these is ", A,"and the smallest number is ", B)
-
-input("Press any key to exit :)")
